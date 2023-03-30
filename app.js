92ada6c186ef6d958382522b9ca902232604f014
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-30 13:49:55 +0300

    add image and auth routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { DB } = process.env;
 
 const express = require("express");
 const cors = require("cors");
@@ -7,12 +8,17 @@ const mongoose = require("mongoose");
 
 const app = express();
 const format = app.get("env") === "development" ? "dev" : "short";
-const { DB } = process.env;
+
+const authRouter = require("./routes/api/auth");
+const userImageRouter = require("./routes/api/userImage");
 
 app.use(cors());
 app.use(express.json());
 app.use(logger(format));
 
 mongoose.connect(DB);
+
+app.use("/api/auth", authRouter);
+app.use("/api/images", userImageRouter);
 
 module.exports = app;
---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/login", () => {});
+router.post("/register", () => {});
+router.get("/current", () => {});
+router.get("/logout", () => {});
+
+module.exports = router;
