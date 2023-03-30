4ce07b9b104a36d0a8f95c062ddbf95d48d676d1
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-30 13:09:13 +0300

    app user model

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
 const format = app.get("env") === "development" ? "dev" : "short";
+const { DB } = process.env;
 
 app.use(cors());
 app.use(express.json());
 app.use(logger(format));
+
+mongoose.connect(DB);
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+const userSchema = new Schema({
+  username: String,
+  email: {
+    type: String,
+    matches: emailRegex,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  accessToken: {
+    type: String,
+    default: null,
+  },
+  verificationToken: {
+    type: String,
+    default: null,
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = { User };
